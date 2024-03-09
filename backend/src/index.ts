0fc3abe70cf2b6ea0f15369a40263fae2c035d32
sahil <sahilcodes23@gmail.com>
2024-03-09 21:45:45 +0530

    Final Code

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
+//writing the middleware
+app.use("/*", cors());
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blogs", blogRouter);
+
+export default app;
